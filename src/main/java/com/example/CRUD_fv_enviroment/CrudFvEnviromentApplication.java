9559f6020db19ede2e13a9f288e35632c5ef988e
Kai_TRD <wkailuo03@gmail.com>
2024-04-25 15:37:48 +0200

    Primer commit

---
+++ src/main/java/com/example/CRUD_fv_enviroment/CrudFvEnviromentApplication.java
@@ -0,0 +1,13 @@
+package com.example.CRUD_fv_enviroment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudFvEnviromentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudFvEnviromentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CRUD_fv_enviroment/CrudFvEnviromentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CRUD_fv_enviroment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudFvEnviromentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
